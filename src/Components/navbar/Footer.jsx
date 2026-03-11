275e409b1e102e03b3daef2e2b30cedf016cc0cc
mr-saquib <mdsaquiba814@gmail.com>
2026-03-12 01:47:20 +0530

    Update Footer.jsx

--- src/Components/navbar/Footer.jsx
+++ src/Components/navbar/Footer.jsx
@@ -15,10 +15,10 @@ const Footer = () => {
             Simplifying business operations with smart software solutions. Helping companies streamline and grow through innovation.
           </p>
           <div className="social-links">
-            <a href="#" aria-label="Facebook"><img src="/src/assets/facebook.png" alt="Facebook" /></a>
-            <a href="#" aria-label="LinkedIn"><img src="/src/assets/linkedin.png" alt="LinkedIn" /></a>
-            <a href="#" aria-label="Twitter"><img src="/src/assets/twitter.png" alt="Twitter" /></a>
-            <a href="https://www.instagram.com/dev_saquib?igsh=MTI4MmduanNtNGwzOQ==" aria-label="Instagram"><img src="/src/assets/instagram.png" alt="Instagram" /></a>
+            <a href="#" aria-label="Facebook"><img src="https://ibb.co/3yNyfJXZ" alt="Facebook" /></a>
+            <a href="#" aria-label="LinkedIn"><img src="https://ibb.co/bRJwKmbz" alt="LinkedIn" /></a>
+            <a href="#" aria-label="Twitter"><img src="https://ibb.co/LXRmzHVB" alt="Twitter" /></a>
+            <a href="https://www.instagram.com/dev_saquib?igsh=MTI4MmduanNtNGwzOQ==" aria-label="Instagram"><img src="https://inquisitive-magenta-kuwsjjwl4t.edgeone.app/instagram.png" alt="Instagram" /></a>
           </div>
         </div>
 
